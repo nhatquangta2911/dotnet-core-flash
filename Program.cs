f417134ae7b29fa19e4b4c30c7b707f842ca8869
Nhat Quang Ta <nhatquangta2911@gmail.com>
2019-06-26 16:09:15 +0700

    Initial Very First .NET Core Project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_core_flash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Abs(-4));
+        }
+    }
+}
---
+++ VeryFirstClass.cs
@@ -0,0 +1,6 @@
+namespace dotnet_core_flash
+{
+    public class VeryFirstClass
+    {
+    }
+}

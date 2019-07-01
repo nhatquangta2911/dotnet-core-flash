2f1cd539ea9149a579bf6366c07274f6c72036fc
NhatQuangTa <nhatquangta2911@gmail.com>
2019-07-01 19:25:00 +0700

    Classes, Constructors & Object Initializer

--- Program.cs
+++ Program.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 
-namespace dotnet_core_flash
+namespace VeryFirstNamespace
 {
     class Program
     {
         static void Main(string[] args)
         {
-            String message = "This is my very first C# program ever.";
-            Console.WriteLine(message);
+            var group = new Group(2911, "Shawn");
+            var post = new Post 
+                        {
+                            Id = 1,
+                            Content = "First Post Ever"
+                        };
+            group.Posts.Add(post);
+            System.Console.WriteLine(group.Id);
+            System.Console.WriteLine(group.Name);
+            System.Console.WriteLine(group.Posts[0].Content);
         }
     }
 }
